53ecaa878d3f2811a142df7184be94b84fb7d7b1
wa-kumi <tkmsrt082@outlook.jp>
2023-01-06 14:26:24 +0900

    add記事投稿作成画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,6 +12,11 @@ class PostController extends Controller
         return view('posts/index')->with(['posts' => $post->getPaginateByLimit(3)]);
     }
     
+    public function create()
+    {
+        return view('posts/create');
+    }
+    
     public function show(Post $post)
     {
         return view ('posts/show')->with(['post' => $post]);
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,6 +8,7 @@
     </head>
     <body>
         <h1>Blog Name</h1>
+        <a href='/posts/create'>create</a>
         <div class='posts'>
             @foreach($posts as $post)
                 <div class='post'>
@@ -16,6 +17,7 @@
                     </a>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
+
             @endforeach
         </div>
         <div class='paginate'>{{ $posts->links() }}</div>
